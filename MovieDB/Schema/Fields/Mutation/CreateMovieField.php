85abda463c4ad8f8534e5b7f6004fec3ee277d44
Kyle Harrison <redactedprofile@gmail.com>
2017-06-22 21:22:38 -0700

    ability to create entities

--- MovieDB/Schema/Fields/Mutation/CreateMovieField.php
+++ MovieDB/Schema/Fields/Mutation/CreateMovieField.php
@@ -2,10 +2,13 @@
 
 namespace MovieDB\Schema\Fields\Mutation;
 
+use MovieDB\Entity\Movie;
 use MovieDB\Schema\Types\MovieType;
 use Youshido\GraphQL\Config\Field\FieldConfig;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Field\AbstractField;
+use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Type\Scalar\StringType;
 
 class CreateMovieField extends AbstractField
 {
@@ -17,12 +20,21 @@ class CreateMovieField extends AbstractField
     public function build(FieldConfig $config)
     {
         $config->addArguments([
-
+            'name' => new NonNullType(new StringType())
         ]);
     }
 
     public function resolve($value, array $args, ResolveInfo $info)
     {
+        global $entityManager;
+
+        $movie = new Movie();
+        $movie->setName($args['name']);
+
+        $entityManager->persist($movie);
+        $entityManager->flush();
+
+        return $movie;
     }
 
 }
--- MovieDB/Schema/Fields/Mutation/CreatePersonField.php
+++ MovieDB/Schema/Fields/Mutation/CreatePersonField.php
@@ -2,10 +2,13 @@
 
 namespace MovieDB\Schema\Fields\Mutation;
 
+use MovieDB\Entity\Person;
 use MovieDB\Schema\Types\PersonType;
 use Youshido\GraphQL\Config\Field\FieldConfig;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Field\AbstractField;
+use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Type\Scalar\StringType;
 
 class CreatePersonField extends AbstractField
 {
@@ -17,12 +20,21 @@ class CreatePersonField extends AbstractField
     public function build(FieldConfig $config)
     {
         $config->addArguments([
-
+            'name' => new NonNullType(new StringType())
         ]);
     }
 
     public function resolve($value, array $args, ResolveInfo $info)
     {
+        global $entityManager;
+
+        $person = new Person();
+        $person->setName($args['name']);
+
+        $entityManager->persist($person);
+        $entityManager->flush();
+
+        return $person;
     }
 
 }
