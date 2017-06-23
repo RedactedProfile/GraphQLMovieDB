c724fceb1ad37783ef848be70f1eb8577d6ed907
Kyle Harrison <redactedprofile@gmail.com>
2017-06-22 20:49:59 -0700

    heroku connection

--- bootstrap.php
+++ bootstrap.php
@@ -11,14 +11,14 @@ $isDevMode = true;
 // the connection configuration
 $dbParams = array(
     'driver'   => 'pdo_pgsql',
-    'user'     => 'postgres',
-    'password' => 'toor',
-    'dbname'   => 'moviedb',
-//    'dbname' => 'da6e552ga9mq1u',
-//    'host' => 'ec2-107-21-108-204.compute-1.amazonaws.com',
-//    'port' => 5432,
-//    'user' => 'njiwaijeffjqdy',
-//    'password' => 'fe669d9ca7c743068e82b6cea32c4c98ccf7f88d4d9084f911ff3fbd5ae21b72'
+//    'user'     => 'postgres',
+//    'password' => 'toor',
+//    'dbname'   => 'moviedb',
+    'dbname' => 'da6e552ga9mq1u',
+    'host' => 'ec2-107-21-108-204.compute-1.amazonaws.com',
+    'port' => 5432,
+    'user' => 'njiwaijeffjqdy',
+    'password' => 'fe669d9ca7c743068e82b6cea32c4c98ccf7f88d4d9084f911ff3fbd5ae21b72'
 );
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
